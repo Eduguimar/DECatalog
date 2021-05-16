98800fa83a4599ee60fbedd422eb21529a66df4f
Eduardo Guimaraes <eduguimara@gmail.com>
2021-05-15 21:12:37 -0300

    Project created

---
+++ backend/src/main/java/com/devedu/decatalog/DecatalogApplication.java
@@ -0,0 +1,13 @@
+package com.devedu.decatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devedu/decatalog/DecatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devedu.decatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
