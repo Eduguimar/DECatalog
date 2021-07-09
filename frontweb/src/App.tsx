c3f8e9fd7e55e1aee0bc8d48cad25a0047c2d173
Eduardo Guimaraes <eduguimara@gmail.com>
2021-07-09 19:26:47 -0300

    Navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,12 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from 'components/Navbar/Navbar';
 
 function App() {
   return (
     <div>
-      <h1>Hello DE Catalog!</h1>
+      <Navbar />
+      <h1>Hello DE Catalog</h1>
     </div>
   );
 }
