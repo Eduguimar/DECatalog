de10c27bca3a5fc412da5bf3ea1ddcb8ffb5bd8d
Eduardo Guimaraes <eduguimara@gmail.com>
2021-07-11 13:52:18 -0300

    Custom ButtomIcon

--- frontweb/src/components/ButtonIcon/ButtonIcon.tsx
+++ frontweb/src/components/ButtonIcon/ButtonIcon.tsx
@@ -1,11 +1,15 @@
 import './styles.css';
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 
 export default function ButtonIcon() {
   return (
-    <>
-      <button className="btn btn-primary btn-icon">
-        INICIE AGORA A SUA BUSCA
+    <div className="btn-container">
+      <button className="btn btn-primary">
+        <h6>Inicie agora a sua busca</h6>
       </button>
-    </>
+      <div className="btn-icon-container">
+        <ArrowIcon />
+      </div>
+    </div>
   );
 }
