5f0e6d9751b96a6b58a250cb79fdda36bcdbab98
Eduardo Guimaraes <eduguimara@gmail.com>
2021-11-27 14:52:46 -0300

    ProductDetails route

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -2,6 +2,7 @@ import Navbar from 'components/Navbar/Navbar';
 import Admin from 'pages/Admin/Admin';
 import Catalog from 'pages/Catalog/Catalog';
 import Home from 'pages/Home/Home';
+import ProductDetails from 'pages/ProductDetails/ProductDetails';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 export default function Routes() {
@@ -12,8 +13,11 @@ export default function Routes() {
         <Route path="/" exact>
           <Home />
         </Route>
-        <Route path="/products">
+        <Route path="/products" exact>
           <Catalog />
+        </Route>
+        <Route path="/products/:productId">
+          <ProductDetails />
         </Route>
         <Route path="/admin">
           <Admin />
