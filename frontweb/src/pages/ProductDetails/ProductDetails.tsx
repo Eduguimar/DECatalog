0a326628b2ff954af469882194be427911d61444
Eduardo Guimaraes <eduguimara@gmail.com>
2021-11-27 22:20:49 -0300

    ProductDetails HTML

--- frontweb/src/pages/ProductDetails/ProductDetails.tsx
+++ frontweb/src/pages/ProductDetails/ProductDetails.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import ProductPrice from 'components/ProductPrice/ProductPrice';
 
 export default function ProductDetails() {
   return (
-    <div>
-      <h1>Página Product Details</h1>
+    <div className="product-details-container">
+      <div className="product-details-card">
+        <div className="goback-container">
+          <ArrowIcon />
+          <h2>VOLTAR</h2>
+        </div>
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="img-container">
+              <img
+                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"
+                alt="Nome do produto"
+              />
+            </div>
+            <div className="name-price-container">
+              <h1>Nome do Produto</h1>
+              <ProductPrice price={2345.6} />
+            </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="description-container">
+              <h2>Descrição do produto</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
+                minima quam repellendus dolor recusandae fugit vitae libero
+                perspiciatis reiciendis dicta.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
