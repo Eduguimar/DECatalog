990d12829aba53fe3a61893d142ca6e7b4b1b0aa
Eduardo Guimaraes <eduguimara@gmail.com>
2021-11-30 21:44:41 -0300

    Admin Navbar mobile layout

--- frontweb/src/pages/Admin/Admin.tsx
+++ frontweb/src/pages/Admin/Admin.tsx
@@ -1,11 +1,14 @@
 import Navbar from './Navbar/Navbar';
 
+import './styles.css';
+
 export default function Admin() {
   return (
     <div className="admin-container">
       <Navbar />
-      <div className="admin-content"></div>
-      <h1>Conteúdo</h1>
+      <div className="admin-content">
+        <h1>Conteúdo</h1>
+      </div>
     </div>
   );
 }
